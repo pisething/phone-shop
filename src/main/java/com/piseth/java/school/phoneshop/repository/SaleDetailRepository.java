f21422eea053e146a73be958c565fb2161f5fa6a
Piseth Ing <piseth.ing@allianz.com>
2023-02-08 11:09:53 +0700

    report version 2

--- src/main/java/com/piseth/java/school/phoneshop/repository/SaleDetailRepository.java
+++ src/main/java/com/piseth/java/school/phoneshop/repository/SaleDetailRepository.java
@@ -4,18 +4,21 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.piseth.java.school.phoneshop.model.SaleDetail;
 import com.piseth.java.school.phoneshop.projections.SaleByDate;
 
-public interface SaleDetailRepository extends JpaRepository<SaleDetail, Long>{
+public interface SaleDetailRepository extends JpaRepository<SaleDetail, Long>, JpaSpecificationExecutor<SaleDetail>{
 
-	@Query(value = "select date(sold_date) soldDate,product_id productId,p.name productName,sum(sd.unit) totalUnit, sum(p.sale_price * sd.unit) amount\r\n"
+	@Query(value = "select date(sold_date) soldDate,product_id productId,p.name productName,sum(sd.unit) totalUnit, sum(sd.amount * sd.unit) amount\r\n"
 			+ "from sales s inner join sale_details sd on s.id = sd.sale_id\r\n"
 			+ "inner join products p on p.id = sd.product_id\r\n"
 			+ "where date(sold_date) = :soldDate\r\n"
 			+ "group by date(sold_date), product_id, p.name", nativeQuery = true)
 	List<SaleByDate> findByProduct(@Param("soldDate") LocalDate soldDate);
+	
+	//List<SaleByDate> findByProductByDate(@Param("soldDate") LocalDate soldDate);
 }
--- src/main/java/com/piseth/java/school/phoneshop/service/ReportingService.java
+++ src/main/java/com/piseth/java/school/phoneshop/service/ReportingService.java
@@ -3,10 +3,13 @@ package com.piseth.java.school.phoneshop.service;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.piseth.java.school.phoneshop.dto.SaleByDateDTO;
 import com.piseth.java.school.phoneshop.projections.SaleByDate;
 
 public interface ReportingService {
 // daily sale by product
 	List<SaleByDate> getProductSoldByDate(LocalDate soldDate);
+	
+	List<SaleByDateDTO> getProductSoldByDateV2(LocalDate soldDate);
 
 }
